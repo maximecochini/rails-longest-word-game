f45c08690733be7ac5e31bfa696aa5040de498e2
Maxime Cochini <maxime.cochini@gmail.com>
2020-05-25 14:49:33 +0200

    Added untracked files

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = 10.times.map { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split(" ")
+    @attempt = params[:word]
+    if match?(@attempt, @letters) == false
+      @result = { score: 0,
+                  resolve: 0,
+                  attempt: @attempt,
+                  letters: @letters }
+    elsif correct?(@attempt)["found"] == false
+      @result = { score: 0,
+                  resolve: 1,
+                  attempt: @attempt,
+                  letters: @letters }
+    else
+      @result = { score: 100,
+                  resolve: 2,
+                  attempt: @attempt,
+                  letters: @letters }
+    end
+  end
+
+  private
+
+  def match?(attempt, grid)
+    lettersattempt = attempt.upcase.split("").sort
+    letterscount = {}
+    ('A'..'Z').each { |letter| letterscount[letter] = 0 + grid.count(letter) - lettersattempt.count(letter) }
+    return letterscount.values.min.negative? ? false : true
+  end
+
+  def correct?(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    dictionary = open(url).read
+    return JSON.parse(dictionary)
+  end
+end
